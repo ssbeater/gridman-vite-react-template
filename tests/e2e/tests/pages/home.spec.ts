ce02e1da2e396ca80ab92f830d1ca9dc7fa82f4d
Beater <santiagosuarez.beater@outlook.com>
2023-10-08 13:08:31 -0500

    test: :test_tube: Add simple cypress test

---
+++ tests/e2e/tests/pages/home.spec.ts
@@ -0,0 +1,8 @@
+describe("The Home Page", () => {
+	it("successfully loads", () => {
+		cy.visit("/");
+		cy.findByRole("heading", {
+			name: /Vite & React Template/i,
+		}).should("exist");
+	});
+});
