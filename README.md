963aba4ad88f110ad9079c1eac8c1a79584ca810
Beater <santiagosuarez.beater@outlook.com>
2023-10-08 08:47:18 -0500

    feat: :tada: First commit - Vite & React Template by Gridman

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+This template by gridman
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+	return (
+		<>
+			<h1>Vite & React Template</h1>
+			<p>By Gridman</p>
+		</>
+	);
+}
